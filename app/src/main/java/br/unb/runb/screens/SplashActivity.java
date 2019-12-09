038bdd1f5e3ef8cb770032451af1da3600e8238f
Bruno Siqueira <brunovsiq@gmail.com>
2019-12-09 15:40:27 -0300

    Splash screen created and setted

---
+++ app/src/main/java/br/unb/runb/screens/SplashActivity.java
@@ -0,0 +1,18 @@
+package br.unb.runb.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
